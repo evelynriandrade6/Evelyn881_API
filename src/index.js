60c9f8b5bb2fe52e79b1fdf7990769c31d419ce6
evelynriandrade6 <eveyandrade0.3@gmail.com>
2024-08-28 13:45:29 -0300

    Atividade de autenticar

--- src/index.js
+++ src/index.js
@@ -11,6 +11,34 @@ class AppController{
         this.express.use(express.json());
     }
     routes(){
+        const users = [];
+        this.express.post("/users",(req,res)=>{
+            const {id, nome, email, senha} = req.body;
+            users.push({id, nome, email, senha});
+            res.status(200).send({message:"Usuário cadastrado com sucesso"});
+        });
+
+        this.express.get("/users/:id",(req,res)=>{
+            const{id} = req.params;
+            const user = users.find((user) => user.id == id);
+            if (user) {
+                res.status(200).send(user);
+            } else {
+                res.status(400).send({message:"Usuário não encontrado"});
+            }
+        });
+
+        this.express.post("/auth",(req,res)=>{
+            const {email, senha} = req.body;
+            const user = users.find((user) => user.email == email && user.senha == senha);
+            console.log(user);
+            if (user) {
+                res.status(200).send({message:"Usuário authenticado com sucesso"});
+            } else {
+                res.status(400).send({message:"Usuário não authenticado"});
+            }
+        });
+
         this.express.get('/health/', (req, res) => {res.send({nome:"Evelyn", idade:"17", CPF: 47806760881});
     });
 
--- src/server.js
+++ src/server.js
@@ -1,3 +1,3 @@
 const app = require("./index");
-// http://localhost:3000/
-app.listen(3000);
+// http://localhost:5000/
+app.listen(5000);
